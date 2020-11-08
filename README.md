94e707eda7e88cf76c5a34326ee7365afa356f55
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-11-07 23:26:33 -0800

    Committed forecast divs in html, callbacks in js, and organzied readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Weather-Dashboard-
+Creating a simple weather and forecast application that allows users to track current weather and see a 5 day forecast. 
+## Overview
+This project is designated of traveling users that are concerned with the weather and forecasts of cities they will be visiting. 
+When the user inputs the city in which they will be traveling to they will receive information of that city that includes:
+
+City name, current date (MM/DD/YYYY),
+Temperature (F),
+Humidity,
+Wind Speed,
+UV Index
+
+In addition, a 5 day forecast that show the following:
+
+Day 1 (MM/DD/YYYY),
+Day 1 Temperature,
+Day 1 Humidity
+
+Day 2 (MM/DD/YYYY),
+Day 2 Temperature,
+Day 2 Humidity
+
+Day 3 (MM/DD/YYYY),
+Day 3 Temperature,
+Day 3 Humidity
+
+Day 4 (MM/DD/YYYY),
+Day 4 Temperature,
+Day 4 Humidity
+
+Day 5 (MM/DD/YYYY),
+Day 5 Temperature,
+Day 5 Humidity
+
+### Application Preview
+
+ ![Weather-Dashboard](./.png)
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,16 @@
-
+// Create APIKey and store it in a variable
 var APIKey = "237dc8df00ed324646bdf99ee09d2439";
-
+// Activate #search-button using .on("click",)eventListener
 $("#search-Btn").on("click", function(){
 
     var city = $("#search-input").val().trim();
       console.log(city);
+    
+
+    // var lat = $(".UV-Index").response.coord.lat; 
+    // var lon = $(".UV-Index").val().trim();
+    // console.log(lat);
+    // console.log(lon);
 
       weather(city);
 });
@@ -17,13 +23,14 @@ function weather(city){
         method: "GET"
     }).then (function(response){
         console.log(response);
-        // callback fivedayweather function
+        // callback fiveDayWeather function
         fiveDayWeather(city);
         // callback UV index function
+        // uvIndex(lat, lon);
         // Transfer content to HTML
         var d = new Date (response.dt * 1000).toLocaleDateString();
         console.log(d);
-          $("#city-name-date").text(response.name + " " + d);
+          $(".city").html("<h1>" + response.name + " " + d + "</h1>");
           $(".humidity").text("Humidity: " + response.main.humidity);
           $(".wind").text("Wind Speed: " + response.wind.speed);
           
@@ -48,6 +55,31 @@ function fiveDayWeather(city){
 
         var listArr = [response.list[0],response.list[8],response.list[16],response.list[24],response.list[32]];
         console.log(listArr);
+        
+        var fc1 = new Date (response.list[0].dt * 1000).toLocaleDateString();
+        var forCas1F = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+        $(".card-body-1").text(fc1);
+        // $(".card1").html("<div>" + forCas1F + "</div>");
+        var fc2 = new Date (response.list[8].dt * 1000).toLocaleDateString();
+        $(".card-body-2").text(fc2);
+        var fc3 = new Date (response.list[16].dt * 1000).toLocaleDateString();
+        $(".card-body-3").text(fc3);
+        var fc4 = new Date (response.list[24].dt * 1000).toLocaleDateString();
+        $(".card-body-4").text(fc4);
+        var fc5 = new Date (response.list[32].dt * 1000).toLocaleDateString();
+        $(".card-body-5").text(fc5);
     })
-  
+    
+    // function uvIndex(lat, lon){
+    //     var UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=&lon=" + lat&lon + "&appid=" + APIKey;
+    //     console.log(UVIndexURL);
+    // }
+    // $.ajax({
+    //     url: UVIndexURL,
+    //     method: "GET"
+
+    // }).then(function(response){
+    //     console.log(response);
+    // })
 }
+  
