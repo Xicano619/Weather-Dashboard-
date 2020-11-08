43e40b395b160ca75a8c3a4a36f66a4f1cee0133
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-11-08 12:20:10 -0800

    Forecast commited

--- assets/javascript.js
+++ assets/javascript.js
@@ -30,16 +30,16 @@ function weather(city){
         // Transfer content to HTML
         var d = new Date (response.dt * 1000).toLocaleDateString();
         console.log(d);
-          $(".city").html("<h1>" + response.name + " " + d + "</h1>");
-          $(".humidity").text("Humidity: " + response.main.humidity);
-          $(".wind").text("Wind Speed: " + response.wind.speed);
+          $(".city").html("<h4>" + response.name + " " + d + "</h4>");
+          $(".humidity").text("Humidity: " + response.main.humidity + "%");
+          $(".wind").text("Wind Speed: " + response.wind.speed + " " + "MPH");
           
          
         //Convert the temp to fahrenheit
           var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
         //   Add temp content to html
-        $(".temp").text("Temperature (F) " + tempF.toFixed(2));
+        $(".temp").text("Temperature: " + tempF.toFixed(2) + "F");
     })
 }
 
@@ -56,18 +56,54 @@ function fiveDayWeather(city){
         var listArr = [response.list[0],response.list[8],response.list[16],response.list[24],response.list[32]];
         console.log(listArr);
         
+        $("#fiveDayFore").html("<h6>" + "5-Day Forecast:</h6>");
+         // Create vars for day2 forecast
         var fc1 = new Date (response.list[0].dt * 1000).toLocaleDateString();
         var forCas1F = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+        var Icon1 = (response.list[0].weather[0].icon);
+        // Add content to html for day1 forecast
         $(".card-body-1").text(fc1);
+        $(".icon1").text(Icon1);
+        $(".temp1").text("Temperature: " + forCas1F.toFixed(2) + "F");
+        $(".hum1").text("Humidity: " + response.list[0].main.humidity + "%");
         // $(".card1").html("<div>" + forCas1F + "</div>");
+        // Create vars for day2 forecast
+        var forCas2F = (response.list[8].main.temp - 273.15) * 1.80 + 32;
+        var Icon2 = (response.list[8].weather[0].icon);
         var fc2 = new Date (response.list[8].dt * 1000).toLocaleDateString();
+        // Add content to html for day2 forecast
         $(".card-body-2").text(fc2);
+        $(".icon2").text(Icon2);
+        $(".temp2").text("Temperature: " + forCas1F.toFixed(2) + "F");
+        $(".hum2").text("Humidity: " + response.list[8].main.humidity + "%");
+        // $(".card2").html("<div>" + forCas2F + "</div>");
+         // Create vars for day3 forecast
         var fc3 = new Date (response.list[16].dt * 1000).toLocaleDateString();
+        var forCas3F = (response.list[16].main.temp - 273.15) * 1.80 + 32;
+        var Icon3 = (response.list[16].weather[0].icon);
+        // Add content to html for day3 forecast
         $(".card-body-3").text(fc3);
+        $(".icon3").text(Icon3);
+        $(".temp3").text("Temperature: " + forCas1F.toFixed(2) + "F");
+        $(".hum3").text("Humidity: " + response.list[16].main.humidity + "%");
+        // Create var for day4 forecast
         var fc4 = new Date (response.list[24].dt * 1000).toLocaleDateString();
+        var forCas4F = (response.list[24].main.temp - 273.15) * 1.80 + 32;
+        var Icon4 = (response.list[24].weather[0].icon);
+        // Add content to html for day4 forecast
         $(".card-body-4").text(fc4);
+        $(".icon4").text(Icon4);
+        $(".temp4").text("Temperature: " + forCas1F.toFixed(2) + "F");
+        $(".hum4").text("Humidity: " + response.list[24].main.humidity + "%");
+        // Create var for day5 forecast
         var fc5 = new Date (response.list[32].dt * 1000).toLocaleDateString();
+        var forCas5F = (response.list[32].main.temp - 273.15) * 1.80 + 32;
+        var Icon5 = (response.list[32].weather[0].icon);
+         // Add content to html for day5 forecast
         $(".card-body-5").text(fc5);
+        $(".icon5").text(Icon5);
+        $(".temp5").text("Temperature: " + forCas1F.toFixed(2) + "F");
+        $(".hum5").text("Humidity: " + response.list[32].main.humidity + "%");
     })
     
     // function uvIndex(lat, lon){
