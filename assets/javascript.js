fdc07737578131ea4b7006bd3699f243d9e728fa
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-11-11 14:05:56 -0800

    final commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,18 +1,16 @@
 // Create APIKey and store it in a variable
 var APIKey = "237dc8df00ed324646bdf99ee09d2439";
+var store = JSON.parse(localStorage.getItem("cities")) || [];
 // Activate #search-button using .on("click",)eventListener
 $("#search-Btn").on("click", function () {
   var city = $("#search-input").val().trim();
   console.log(city);
 
-  // var lat = $(".UV-Index").response.coord.lat;
-  // var lon = $(".UV-Index").val().trim();
-  // console.log(lat);
-  // console.log(lon);
+  
 
   weather(city);
 
-  save();
+  save(city);
 });
 
 function weather(city) {
@@ -30,7 +28,7 @@ function weather(city) {
     // callback fiveDayWeather function
     fiveDayWeather(city);
     // callback UV index function
-    // uvIndex(lat, lon);
+    uvIndex(response.coord.lat, response.coord.lon);
     // Transfer content to HTML
     var d = new Date(response.dt * 1000).toLocaleDateString();
     console.log(d);
@@ -42,7 +40,7 @@ function weather(city) {
     var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
     //   Add temp content to html
-    $(".temp").text("Temperature: " + tempF.toFixed(2) + "F");
+    $(".temp").text("Temperature: " + tempF.toFixed(2) + "&#176 F");
   });
 }
 
@@ -76,8 +74,8 @@ function fiveDayWeather(city) {
     var Icon1 = response.list[0].weather[0].icon;
     // Add content to html for day1 forecast
     $(".card-body-1").text(fc1);
-    $(".icon1").text(Icon1);
-    $(".temp1").text("Temperature: " + forCas1F.toFixed(2) + "F");
+    $(".icon1").attr("src", "http://openweathermap.org/img/wn/" + Icon1 + "@2x.png");
+    $(".temp1").text("Temperature: " + forCas1F.toFixed(2) + "&#176 F");
     $(".hum1").text("Humidity: " + response.list[0].main.humidity + "%");
     // $(".card1").html("<div>" + forCas1F + "</div>");
     // Create vars for day2 forecast
@@ -86,8 +84,8 @@ function fiveDayWeather(city) {
     var fc2 = new Date(response.list[8].dt * 1000).toLocaleDateString();
     // Add content to html for day2 forecast
     $(".card-body-2").text(fc2);
-    $(".icon2").text(Icon2);
-    $(".temp2").text("Temperature: " + forCas1F.toFixed(2) + "F");
+    $(".icon2").attr("src", "http://openweathermap.org/img/wn/" + Icon2 + "@2x.png");
+    $(".temp2").text("Temperature: " + forCas1F.toFixed(2) + "&#176 F");
     $(".hum2").text("Humidity: " + response.list[8].main.humidity + "%");
     // $(".card2").html("<div>" + forCas2F + "</div>");
     // Create vars for day3 forecast
@@ -96,8 +94,8 @@ function fiveDayWeather(city) {
     var Icon3 = response.list[16].weather[0].icon;
     // Add content to html for day3 forecast
     $(".card-body-3").text(fc3);
-    $(".icon3").text(Icon3);
-    $(".temp3").text("Temperature: " + forCas1F.toFixed(2) + "F");
+    $(".icon3").attr("src", "http://openweathermap.org/img/wn/" + Icon3 + "@2x.png");
+    $(".temp3").text("Temperature: " + forCas1F.toFixed(2) + "&#176 F");
     $(".hum3").text("Humidity: " + response.list[16].main.humidity + "%");
     // Create var for day4 forecast
     var fc4 = new Date(response.list[24].dt * 1000).toLocaleDateString();
@@ -105,8 +103,8 @@ function fiveDayWeather(city) {
     var Icon4 = response.list[24].weather[0].icon;
     // Add content to html for day4 forecast
     $(".card-body-4").text(fc4);
-    $(".icon4").text(Icon4);
-    $(".temp4").text("Temperature: " + forCas1F.toFixed(2) + "F");
+    $(".icon4").attr("src", "http://openweathermap.org/img/wn/" + Icon4 + "@2x.png");
+    $(".temp4").text("Temperature: " + forCas1F.toFixed(2) + "&#176 F");
     $(".hum4").text("Humidity: " + response.list[24].main.humidity + "%");
     // Create var for day5 forecast
     var fc5 = new Date(response.list[32].dt * 1000).toLocaleDateString();
@@ -114,31 +112,40 @@ function fiveDayWeather(city) {
     var Icon5 = response.list[32].weather[0].icon;
     // Add content to html for day5 forecast
     $(".card-body-5").text(fc5);
-    $(".icon5").text(Icon5);
-    $(".temp5").text("Temperature: " + forCas1F.toFixed(2) + "F");
+    $(".icon5").attr("src", "http://openweathermap.org/img/wn/" + Icon5 + "@2x.png");
+    $(".temp5").text("Temperature: " + forCas1F.toFixed(2) + "&#176 F");
     $(".hum5").text("Humidity: " + response.list[32].main.humidity + "%");
   });
-
-  // function uvIndex(lat, lon){
-  //     var UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=&lon=" + lat&lon + "&appid=" + APIKey;
-  //     console.log(UVIndexURL);
-  // }
-  // $.ajax({
-  //     url: UVIndexURL,
-  //     method: "GET"
-
-  // }).then(function(response){
-  //     console.log(response);
-  // })
-
-//   function save() {
-//     var saveCity = $(this).parent().data(city);
-//     var text = $(this).siblings("#city-section").val();
-
-//     localStorage.setItem(saveCity, text);
-//   }
-
-//   $("#city-section[data-store='cities'] text").val(
-//     localStorage.getItem("cities")
-//   );
 }
+function uvIndex(lat, lon) {
+  var UVIndexURL =
+    "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
+  console.log(UVIndexURL);
+
+  $.ajax({
+    url: UVIndexURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    $(".UV-Index").text("UV-Index: " + response.value);
+  });
+}
+  function save(city) {
+    store.push(city);
+
+    localStorage.setItem("cities", JSON.stringify(store));
+  }
+
+  for(var i = 0; i < store.length; i++) {
+    var Btn = $("<button>").text(store[i]);
+    $("#city-section").append(Btn);
+  }
+
+  // $("#city-section").on("click", "button", function () {
+
+  // }
